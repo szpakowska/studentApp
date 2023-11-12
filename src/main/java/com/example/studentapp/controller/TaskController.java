9afa85d16bb6a2fa31098b9f4044be01fc54a79d
anna-szpakowska <annaszpakowska@outlook.com>
2023-11-12 12:44:05 +0100

    Initial version

---
+++ src/main/java/com/example/studentapp/controller/TaskController.java
@@ -0,0 +1,23 @@
+package com.example.studentapp.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+
+
+    @GetMapping("/tasks")
+    public String getTaskList() {
+        return "tasks/tasks";
+    }
+
+
+    @GetMapping("/addTask")
+    public String getAddTask() {
+        return "tasks/addTask";
+    }
+
+
+}
